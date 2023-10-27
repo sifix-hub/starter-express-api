26bfda7de5b90db1b591737e2584293631e167a4
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2023-10-27 17:56:36 +0100

    routes added

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,9 @@ const router = express.Router();
 const { authorizeUser, isEmailVerified } = require('../middlewares/authorize')
 const { register, verifyEmail, login } = require('../controllers/user')
 
+router.post('/register', register)
+router.post('/login', login)
+router.post('/verify-email', authorizeUser, verifyEmail);
 
+module.exports = router;
 
